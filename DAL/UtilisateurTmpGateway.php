735ab381bf472b0cfe505dcea518bbe9c31c7c6c
chcurmont <charly.curmont@etu.udamail.fr>
2017-03-20 10:57:42 +0100

    Version un peu plus fonctionnelle

---
+++ DAL/UtilisateurTmpGateway.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Charly
+ * Date: 13/03/2017
+ * Time: 08:28
+ */
+
+namespace DAL;
+
+
+class UtilisateurTmpGateway
+{
+
+}
---
+++ metier/Utilisateurtmp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Charly
+ * Date: 06/03/2017
+ * Time: 10:51
+ */
+
+namespace metier;
+
+
+class Utilisateurtmp
+{
+
+}
